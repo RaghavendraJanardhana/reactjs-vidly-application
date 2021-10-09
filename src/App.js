20753797654cee4e35e437aaf523f506bf3fd2ce
Raghavendra J <Raghavendra.Janardhana@tecnotree.com>
2021-10-09 12:13:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React,{Component} from "react";
+import {Route,Redirect,Switch} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
+import ProtectedRoute from './components/common/protectedRoute'
+import Movies from "./components/movies"
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import Logout from "./components/common/logout";
+import RegisterForm from "./components/registerForm";
+
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css';
+import auth from "./services/authService";
+
+
+class App extends Component {
+  state ={};
+  componentDidMount(){
+
+   const user= auth.getCurrentUser();
+   this.setState({user});
+    
+  }
+  render(){
+    const {user} =this.state;
+    return (
+      <React.Fragment>
+      <ToastContainer/>
+     <NavBar user={user}/>
+      <main className="container">
+      <Switch>
+      <Route path="/login" component={LoginForm}></Route>
+      <Route path="/logout" component={Logout}></Route>
+      <Route path="/register" component={RegisterForm}></Route>
+      <ProtectedRoute path="/movies/:id" component={MovieForm}/>
+      <Route path="/movies" render={props => <Movies {...props} user={this.state.user}/>}></Route>
+      <Route path="/customers" component={Customers}></Route>
+      <Route path="/rentals" component={Rentals}></Route>
+      <Route path="/not-found" component={NotFound}></Route>
+      <Redirect from="/" exact  to="/movies" />
+      <Redirect to="/not-found"/>
+      </Switch>
+      </main>      
+      </React.Fragment>
+     );
+  }
+  
+}
+
+export default App;
